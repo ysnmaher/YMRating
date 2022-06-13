467e33ceb75715eff7d43697754457ab56dc9351
ysnmaher <ysnmaher@gmail.com>
2022-06-13 12:42:01 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YMRating
+
+A description of this package.
---
+++ Sources/YMRating/YMRating.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  YMrating
+//
+//  Created by yassin maher mohamed on 13/06/2022.
+//
+
+import SwiftUI
+
+public struct YMRating: View {
+    @Binding var selected:Int
+    var maxRating:Int
+    var width:CGFloat
+    var color : UIColor = .red
+    var fillSymbol :String
+    var emptySymbol :String
+    public init(maxRating:Int,selected:Binding<Int>,width:CGFloat=30,color:UIColor=UIColor.systemPurple,fillSymbol:String="heart.fill",emptySymbol:String="heart") {
+        self.maxRating = maxRating
+        self._selected = selected
+        self.width = width
+        self.emptySymbol = emptySymbol
+        self.fillSymbol = fillSymbol
+        
+    }
+    
+   public var body: some View {
+        HStack {
+            ForEach(0..<maxRating){ index in
+
+                Image(systemName:index < selected ? fillSymbol : emptySymbol )
+                        .resizable()
+                        .foregroundColor(Color(self.color))
+                        .frame(width:width,height:width)
+                        .onTapGesture {
+//                            print(index)
+                            selected = index+1
+                        }
+
+            }
+
+
+        }
+        
+    }
+}
+
+struct YMRating_Previews: PreviewProvider {
+    static var previews: some View {
+        YMRating(maxRating: 5, selected: .constant(3))
+    }
+}
